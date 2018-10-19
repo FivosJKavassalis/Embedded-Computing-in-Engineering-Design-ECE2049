c2048d916a058edd3075b28e07179906ecc20422
Fivos Kavassalis <fikavassalis@wpi.edu>
2018-10-19 15:10:31 +0300

    Add files via upload

---
+++ ece2049a18_lab3_grodriguez_fikavassalis/lab3_grodriguez_fikavassalis/frequencies.h
@@ -0,0 +1,24 @@
+
+#ifndef FREQUENCIES_H_
+#define FREQUENCIES_H_
+
+
+#include <msp430.h>
+
+#define Alo 440
+#define Bh 466
+#define B 494
+#define C 523
+#define Cs 554
+#define D 587
+#define Eh 622
+#define E 659
+#define F 698
+#define Fs 740
+#define G 784
+#define Ah 831
+#define Ahi 880
+
+
+
+#endif /* FREQUENCIES_H_ */
